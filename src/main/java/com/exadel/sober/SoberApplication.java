0c32c058eb4ea0baaa48c4333999f130aec9fd64
Роман Неред <romaus@mail.ru>
2022-07-18 14:31:32 +0300

    init

---
+++ src/main/java/com/exadel/sober/SoberApplication.java
@@ -0,0 +1,13 @@
+package com.exadel.sober;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoberApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exadel/sober/SoberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exadel.sober;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
